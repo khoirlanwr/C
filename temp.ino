468214abd2c0f93c93f424eacde9a2467d88990e
khoirlanwr <khoirulanwar.x22@gmail.com>
2018-02-26 05:14:49 +0700

    Update temp.ino

--- temp.ino
+++ temp.ino
@@ -1,6 +1,9 @@
-#include <ESP8266WiFi.h>
-#include <DHT.h>
+#include "ESP8266WiFi.h"
+#include "DHT.h"
+#define DHTPIN 5
+#define DHTTYPE DHT11
 
+DHT dht(DHTPIN, DHTTYPE);
 String apiWriteKey = "OU7ZSRFMHX3J22BF"; 
 const char* ssid = "ADVAN S5E 4GS";
 const char* pass = "844429e958d8";
@@ -8,42 +11,25 @@ const char* pass = "844429e958d8";
 const char* server = "api.thingspeak.com";
 WiFiClient client;
 
-#define DHTPIN 5
-#define DHTTYPE DHT11
-DHT dht(DHTPIN, DHTTYPE);
-
 void setup() {
-  // put your setup code here, to run once:
   Serial.begin(9600);
+  Serial.println("DHT Test");
   
-  WiFi.disconnect();
-  delay(10);
-  WiFi.begin(ssid, pass);
-
-  Serial.println();
-  Serial.println();
-  Serial.print("NodeMCU Connecting to WiFi....");
-
-  WiFi.begin(ssid, pass);
   dht.begin();
-  while(WiFi.status() != WL_CONNECTED) {
-    delay(500);
-    Serial.print(".");
+  WiFi.begin(ssid, pass);
+
+  if(WiFi.status() != WL_CONNECTED) {
+    Serial.print("Not Connected!");
   }
-  Serial.println("");
-  Serial.print("NodeMCU Connected to ");
-  Serial.println(ssid);
-  Serial.println();
 }
 
 void loop() {
-  // put your main code here, to run repeatedly:
   float h = dht.readHumidity();
   float t = dht.readTemperature();
-  
+
   if(isnan(t) || isnan(h)) {
     Serial.println("Failed to read from DHT");
-  }else {
+  } else {
     Serial.print("Humidity: ");
     Serial.print(h);
     Serial.print(" %\t");
@@ -51,7 +37,7 @@ void loop() {
     Serial.print(t);
     Serial.println(" *C");
   }
-  
+
   if(client.connect(server, 80)) 
   {
     String tsData = apiWriteKey;
@@ -79,3 +65,4 @@ void loop() {
   Serial.println();
   delay(15000);
 }
+
